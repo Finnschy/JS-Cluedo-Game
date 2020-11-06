f6be52ce41e9de58f9cb9e30514669a9bf862eac
Finnschy <skylerfearless@yahoo.de>
2020-11-06 16:59:26 +0100

    html und css done, js fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,68 +5,67 @@
 let suspectsArray = [
 
     {   
-        nickName: "mrGreen",
         firstName: "Jacob",
         lastName: "Green",
         occupation: "Entrepeneur", 
         age: 45,
         description: "He has a lot of connections.",
-        img: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        img: "/assets/img/cluedo_Charaktere_0003_jacobGreen.png",
         color: "green"
     },
 
     {
-        nickName: "drOrchid",
+
         firstName: "Doctor",
         lastName: "Orchid",
         occupation: "Scientist", 
         age: 26,
         description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy.",
-        img: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        img: "/assets/img/cluedo_Charaktere_0004_doctorOrchid.png",
         color: "white"
     },
 
     {
-        nickName: "profPlum",
+
         firstName: "Victor",
         lastName: "Plum",
         occupation: "Designer", 
         age: 22,
         description: "Billionaire video game designer.",
-        img: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+        img: "/assets/img/cluedo_Charaktere_0005_victorPlum.png",
         color: "purple"
     }, 
 
     {
-        nickName: "missScarlet",
+
         firstName: "Kasandra",
         lastName: "Scarlet",
         occupation: "Actress", 
         age: 21,
         description: "She is an A-list movie star with a dark past.",
-        img: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        img: "/assets/img/cluedo_Charaktere_0001_kasandraScarlet.png",
         color: "red"
     },
 
     {
-        nickName: "mrsPeacock",
+
         firstName: "Eleanor",
         lastName: "Peacock",
         occupation: "Socialité", 
         age: 36,
         description: "She is from a wealthy family and uses her status and money to earn popularity.",
-        img: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        img: "/assets/img/cluedo_Charaktere_0002_eleanorPeacock.png",
         color: "blue"
     }, 
 
     {
-        nickName: "mrMustard",
+
         firstName: "Jack",
         lastName: "Mustard",
         occupation: "Retired Football player", 
         age: 62,
         description: "He is a former football player who tries to get by on his former glory.",
-        img: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+        img: "/assets/img/cluedo_Charaktere_0000_jackMustard.png",
         color: "yellow"
     }, 
 ]
@@ -121,10 +120,9 @@ function pickMystery() {
     // console.log(`${randomSus.firstName} ${randomSus.lastName}`);
     // console.log(randomWeapon.name);
     // console.log(randomRoom.name);
-    let mystery = [`${randomSus.firstName} ${randomSus.lastName} killed Mr. Boddy with a ${randomWeapon.name} in the ${randomRoom.name}`];
-    // console.log(mystery);
-    result.innerHTML = mystery
+    let mystery = [randomSus, randomWeapon, randomRoom]
+    result.innerHTML = [`${mystery[0].firstName} ${mystery[0].lastName} killed Mr. Boddy with a ${mystery[1].name} in the ${mystery[2].name}`];
 }
-pickMystery()
+
 
 
